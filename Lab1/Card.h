cc834d32c4de5540086f21bdbd57bae4b6d570af
Tasia <nastia.domra@gmail.com>
2025-10-01 14:31:52 +0300

    Added Card.cpp and Card.h

--- Lab1/Card.h
+++ Lab1/Card.h
@@ -1,1 +1,23 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <compare> 
+#include <vector>
+using namespace std;
+class Card {
+public:
+    int suit;  
+    int rank;  
+
+    Card(int s = 0, int r = 0) : suit(s), rank(r) {}
+    std::strong_ordering operator<=>(const Card& other) const;
+
+
+    friend ostream& operator<<(ostream& os, const Card& c) {
+
+        os <<"suit: " << c.suit << "rank: " << c.rank;
+        return os;
+    }
+};
+
+void analyzeStacks(const vector<int>& stacks);
